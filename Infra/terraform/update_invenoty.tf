a42f6cc1c0a79a06acea1f4449f679b4c9ec9368
Akshat Verma <vermaakshat420@gmail.com>
2026-07-12 23:25:03 +0530

    feat: add infrastructure configuration for dynamic Ansible inventory generation

--- Infra/terraform/update_invenoty.tf
+++ Infra/terraform/update_invenoty.tf
@@ -1,7 +1,7 @@
-resource "local_file" "inventory" {
-  content = templatefile("${path.module}/inventory.yml.tpl", {
-    managed_one_ip = aws_instance.ansible["managed_one"].private_ip
-    managed_two_ip = aws_instance.ansible["managed_two"].private_ip
-  })
-  filename = "${path.module}/../../01_Variables_Facts_Jinja2/inventory/inventory.yml"
-}
+# resource "local_file" "inventory" {
+#   content = templatefile("${path.module}/inventory.yml.tpl", {
+#     managed_one_ip = aws_instance.ansible["managed_one"].private_ip
+#     managed_two_ip = aws_instance.ansible["managed_two"].private_ip
+#   })
+#   filename = "${path.module}/../../01_Variables_Facts_Jinja2/inventory/inventory.yml"
+# }
